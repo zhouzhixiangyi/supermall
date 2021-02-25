7fc7b04b30f1a15fb5f1ffdb988dbfde1f53d94c
zhouzx <2556835759@qq.com>
2021-02-25 08:05:17 +0800

    首页开发

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,40 +1,72 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    
-    <home-swiper :banners="banners"/>
 
-    <recommend-view :recommends="recommends"/>
+    <home-swiper :banners="banners" />
 
-    <feature-view/>
+    <recommend-view :recommends="recommends" />
+
+    <feature-view />
+
+    <tab-control
+      :titles="['流行', '新款', '精选']"
+      class="tab-control"
+    ></tab-control>
+
+    <div style="height: 1000px"></div>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
-import HomeSwiper from "./childComps/HomeSwiper"
-import RecommendView from "./childComps/RecommendView"
-import FeatureView from "./childComps/FeatureView"
-import { getHomeMultiData } from "network/home";
+import TabControl from "components/content/tabControl/TabControl";
+
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
+
+import { getHomeMultiData, getHomeGoods } from "network/home";
 export default {
   data() {
     return {
       banners: [],
       recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sells: { page: 0, list: [] },
+      },
     };
   },
   components: {
     NavBar,
+    TabControl,
     HomeSwiper,
     RecommendView,
     FeatureView,
   },
   created() {
-    getHomeMultiData().then((res) => {
-      console.log(res);
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    });
+    this.getHomeMultiData();
+
+  },
+  methods: {
+    // 获取导航数据
+    getHomeMultiData() {
+      getHomeMultiData().then((res) => {
+        //console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    // 获取首页商品数据
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1
+      getHomeGoods(type, page).then((res) => {
+        console.log(res);
+        this.goods[type].list.push(...res.data.list)
+        this.goods[page].page = page
+      });
+    },
   },
 };
 </script>
@@ -54,6 +86,9 @@ export default {
   right: 0;
 
   z-index: 9;
-
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
